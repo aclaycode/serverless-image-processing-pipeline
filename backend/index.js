d58395cbdce79078e4954b43e33fc01116e9f5c4
AJ Clay <aj-clay@outlook.com>
2025-06-20 15:42:17 -0500

    Restructure project to separate Lambda functions for file input/output; add sharp layer

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const AWS = require('aws-sdk'); // AWS SDK for interacting with AWS services
-const S3 = new AWS.S3(); // Creates an S3 client to upload/download files
-const sharp = require('sharp'); // Image processing library. This will be used to convert the files
-
-// Lambda funciton main handler. AWS automatically invokes this function when an event occurs.
-// In this case an S3 file upload.
-exports.handler = async (event) => {
-
-};
---
+++ backend/presigned_Upload_URL/presignedURL.js
@@ -0,0 +1,7 @@
+const AWS = require('aws-sdk'); // AWS SDK for interacting with AWS services. Don't need to install aws-sdk into directory by using this
+
+// Lambda funciton main handler. AWS automatically invokes this function when an event occurs.
+// In this case an S3 file upload.
+exports.handler = async (event) => {
+
+};
